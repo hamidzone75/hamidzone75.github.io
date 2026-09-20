176e20fae51d63bc8dd9d133bcc5027bb40ca358
hamidzone75 <hamidzone75@gmail.com>
2026-09-21 01:35:26 +0330

    Add files via upload

---
+++ offline-project-manager/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/offline-barcode-scanner/sw.js', { scope: '/offline-barcode-scanner/' })})}
