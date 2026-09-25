ddaaff21e4b4e98af3aec974ac107271d87ba385
hamidzone75 <hamidzone75@gmail.com>
2026-09-25 20:04:56 +0330

    Delete prjmgr directory

--- prjmgr/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Database module public API
- */
-
-export { db, ensureDefaultSettings, clearAllData, getStorageEstimate } from './database';
-export * from './projectService';
-export { SCHEMA_VERSION, STORES } from './schema';
--- prjmgr/src/engines/date/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './jalali';
-export { DateService, dateService } from './DateService';
-export type { AppDate, CalendarSystem } from './DateService';
